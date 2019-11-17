bfa695d210613422e6772d76c072f38c4aefa7e7
Ajay <majay1638@gmail.com>
2019-11-17 10:45:40 +0530

    added gener category

--- src/component/Card.component.jsx
+++ src/component/Card.component.jsx
@@ -4,15 +4,44 @@ import { Link } from 'react-router-dom';
 import './Card.style.scss'
 class Card extends Component {
 
-    // getGeners(ids){
-    //     fetch('')
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            gener: []
+        }
+    }
+
+    getGeners(ids = [12, 18, 878]) {
+        fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=209e42e2c66ab2cba7c280981a877ace&language=en-US').then(data => data.json()).then(data => {
+            // console.log(data['genres'][id]);
+
+            let generData = [];
+            let i = 0;
+            data['genres'].map((data) => {
+                if (data.id === ids[i]) {
+                    generData.push(data.name);
+                    i++;
+
+                }
+            });
+
+
+            this.setState({ gener: generData });
+
+        }
+        )
+    }
+
+    componentDidMount() {
+
+        this.getGeners(this.props.data.genre_ids)
+
+    }
 
     render() {
         let data = this.props.data;
-        console.log(data);
-
         let backdropIMG = 'https://image.tmdb.org/t/p/original';
+
         return (
 
             <div className="movie_card">
@@ -22,7 +51,7 @@ class Card extends Component {
                         <Link to={`/movie/${data.id}`}><h1>{data.title ? data.title : data.original_name}</h1></Link>
                         <h4>Released : {data.release_date ? data.release_date.split('-')[0] : 'Unknown'}</h4>
                         {data.runtime ? (<span className="minutes"> Runtime :  {data.runtime} min</span>) : ''}
-                        <p className="type">Action, Crime, Fantasy</p>
+                        <p className="type">{this.state.gener.map(gener => (`${gener} , `))} </p>
                     </div>
                 </div>
                 <div className="movie_desc">
--- src/component/cardList.component.jsx
+++ src/component/cardList.component.jsx
@@ -6,10 +6,9 @@ class CardList extends Component {
 
     render() {
         let Moviedata = this.props.data;
-
         return (
             <section className="movies">
-                {Moviedata.map(data => <Card key={data.id} data={data} isSinglePage={false} />)}
+                {Moviedata.map(data => <Card key={data.id} data={data} isSinglePage={false} genersId={data.genre_ids} />)}
             </section>
         )
     }
